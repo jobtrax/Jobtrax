a35b8fa292f8c98245b27868e3f55a08e3dc0f22
Samuel Keer <sirpizzalot@gmail.com>
2019-06-05 15:44:33 -0500

    styling nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/components/home/home.component';
+import { PreviousApplicationsComponent } from '../app/components/previous-applications/previous-applications.component';
+import { FutureApplicationsComponent } from '../app/components/future-applications/future-applications.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'home', component: HomeComponent},
+{path: 'previousApps', component: PreviousApplicationsComponent},
+{path: 'futureApps', component: FutureApplicationsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
