f1414ed87fc0ee64b8dc757b31e6440a58037de0
queyrouzec <queyrouzec@gmail.com>
2019-06-30 14:29:40 -0500

    Basic Serializers created

--- jobfinder/serializers.py
+++ jobfinder/serializers.py
@@ -1,13 +1,32 @@
 from rest_framework import serializers
-from .models import Jobtrax_user, Applications
+import models
+
 
 class Jobtrax_userSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Jobtrax_user
+        model = models.Jobtrax_user
         fields = ('id', 'name', 'email')
+
+
+class CompaniesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Companies
+        fields = '__all__'
+
+
+class Company_contactsSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = models.Company_contacts
+        fields = '__all__'
+
+
+class Job_emailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Job_emails
+        fields = '__all__'
+
 
 class ApplicationsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Applications
+        model = models.Applications
         fields = '__all__'
-
