b37900d3f54522b835e768dd7a9902d66eb6a4ee
Samuel Keer <sirpizzalot@gmail.com>
2019-07-26 15:59:49 -0500

    bypassing cors server side

--- jobtrax/settings.py
+++ jobtrax/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'kou9hk_y%vl-!t452!@c#h9*njhwq1%8l+f59i^^l!10sjq308'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'jobtrax.urls'
